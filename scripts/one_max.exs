9e962a8fd499e82877db40c695ffddd2a30d1400
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2023-01-28 20:14:41 +0800

    misc
    Signed-off-by: Lîm Tsú-thuàn <dannypsnl@gmail.com>

--- scripts/one_max.exs
+++ scripts/one_max.exs
@@ -2,18 +2,18 @@ defmodule OneMax do
   @behaviour Problem
   alias Types.Chromosome
 
-  @impl true
+  @impl Problem
   def genotype() do
     genes = for _ <- 1..1000, do: Enum.random(0..1)
     %Chromosome{genes: genes, size: 42}
   end
 
-  @impl true
+  @impl Problem
   def fitness_function(chromosome) do
     Enum.sum(chromosome.genes)
   end
 
-  @impl true
+  @impl Problem
   def terminate?([best | _]) do
     best.fitness == 1000
   end
