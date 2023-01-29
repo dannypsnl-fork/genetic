d63206ed58a4cd14920bab2337b17b8be521e2da
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2023-01-29 14:20:23 +0800

    add `Enumerable` impl for `Chromosome`
    Signed-off-by: Lîm Tsú-thuàn <dannypsnl@gmail.com>

--- lib/types/chromosome.ex
+++ lib/types/chromosome.ex
@@ -9,3 +9,21 @@ defmodule Types.Chromosome do
   @enforce_keys :genes
   defstruct [:genes, size: 0, fitness: 0, age: 0]
 end
+
+defimpl Enumerable, for: Types.Chromosome do
+  def count(t) do
+    Enumerable.count(t.genes)
+  end
+
+  def member?(t, term) do
+    Enumerable.member?(t.genes, term)
+  end
+
+  def reduce(t, acc, reducer) do
+    Enumerable.reduce(t.genes, acc, reducer)
+  end
+
+  def slice(t) do
+    Enumerable.slice(t.genes)
+  end
+end
