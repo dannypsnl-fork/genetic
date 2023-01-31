d49eda06262ccacd1e2a2e2147a8298959b37fa8
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2023-01-31 12:41:42 +0800

    fix one_max with a special crossover & selection_rate
    Signed-off-by: Lîm Tsú-thuàn <dannypsnl@gmail.com>

--- scripts/one_max.exs
+++ scripts/one_max.exs
@@ -18,8 +18,25 @@ defmodule OneMax do
     best = Enum.max_by(population, &fitness_function/1)
     best.fitness == 1000
   end
+
+  def crossover(p1, p2) do
+    {c1, c2} =
+      Enum.zip(p1.genes, p2.genes)
+      |> Enum.map(fn {x, y} ->
+        v = Bitwise.bor(x, y)
+        {v, v}
+      end)
+      |> Enum.unzip()
+
+    {%Chromosome{genes: c1, size: length(c1)}, %Chromosome{genes: c2, size: length(c2)}}
+  end
 end
 
-soln = Genetic.run(OneMax, crossover_type: &Toolbox.Crossover.single_point/2)
+soln =
+  Genetic.run(OneMax,
+    crossover_type: &OneMax.crossover/2,
+    selection_rate: 0.9
+  )
+
 IO.write("\n")
 IO.inspect(soln)
